a51b59220f149de6e3017370f2b91ac2b564bd54
luwei <892244214@qq.com>
2018-10-29 23:05:13 +0800

    init

---
+++ app/src/main/java/com/tencent/zy/ui/activity/BaseActivity.java
@@ -0,0 +1,30 @@
+package com.tencent.zy.ui.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.tencent.zy.manager.BusManger;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by bryonliu on 2016/1/25.
+ */
+public class BaseActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BusManger.getInstance().register(this);
+    }
+
+    public void setContentView(int layout) {
+        super.setContentView(layout);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        BusManger.getInstance().unregister(this);
+    }
+}
